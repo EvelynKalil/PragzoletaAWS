8311ada59b517a45479cab23e244c6aa971f6188
EvelynKalil <evelyn.rendon@pragma.com.co>
2025-07-08 14:17:51 -0500

    fix: forzar tipado de amplifyconfiguration como ResourcesConfig

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client';
 import './styles/main.scss';
 import App from './App';
 
-import { Amplify } from 'aws-amplify';
-import amplifyconfig from './amplifyconfiguration.json';
+import { Amplify } from 'aws-amplify'
+import type { ResourcesConfig } from '@aws-amplify/core'
+import config from './amplifyconfiguration.json' assert { type: 'json' }
 
-Amplify.configure(amplifyconfig);
+Amplify.configure(config as unknown as ResourcesConfig)
+
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
